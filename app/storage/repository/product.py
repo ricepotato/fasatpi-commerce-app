07cb338f7412bddc4f0a0dd74397990a7e05600e
ricepotato <sukjun40@naver.com>
2026-02-09 02:23:56 +0900

    Refactor ProductRepository to use session maker for database operations; update test to reflect changes in repository initialization

--- app/storage/repository/product.py
+++ app/storage/repository/product.py
@@ -1,20 +1,24 @@
 import sqlalchemy as sa
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from app.enums.entity_status import EntityStatus
 from app.storage.model.product import Product
 
 
 class ProductRepository:
-    def __init__(self, session: AsyncSession):
-        self.session = session
+    def __init__(self, session_maker: async_sessionmaker[AsyncSession]):
+        self.session_maker = session_maker
 
     async def save(self, product: Product):
-        self.session.add(product)
-        self.session.commit()
+        async with self.session_maker.begin() as session:
+            session.add(product)
+            session.commit()
 
     async def find_by_id_in_and_status(
         self, ids: list[int], status: EntityStatus
     ) -> list[Product]:
-        query = sa.select(Product).where(Product.id.in_(ids), Product.status == status)
-        result = await self.session.execute(query)
-        return result.scalars().all()
+        async with self.session_maker.begin() as session:
+            query = sa.select(Product).where(
+                Product.id.in_(ids), Product.status == status
+            )
+            result = await session.execute(query)
+            return result.scalars().all()
--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -7,7 +7,6 @@ from app.enums.entity_status import EntityStatus
 
 @pytest.mark.asyncio
 async def test_find_product(session_maker: async_sessionmaker[AsyncSession]):
-    async with session_maker.begin() as session:
-        repo = product.ProductRepository(session)
-        items = await repo.find_by_id_in_and_status([1], EntityStatus.ACTIVE.value)
-        assert len(items) == 0
+    repo = product.ProductRepository(session_maker)
+    items = await repo.find_by_id_in_and_status([1], EntityStatus.ACTIVE.value)
+    assert len(items) == 0
